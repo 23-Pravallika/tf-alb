16c036ce8666795ea8b7a9264e68c1692ec8d305
23-Pravallika <learngain23@gmail.com>
2024-02-23 11:38:44 +0530

    AutoPush

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,5 @@ output "ALB_ARN"{
     value = aws_lb.alb.arn
 }
 
-output "PUBLIC_ALB_INTERNAL" {
-    value = aws_lb.alb.ALB_INTERNAL
-}
-
-output "PRIVATE_ALB_INTERNAL" {
-    value = aws_lb.alb.ALB_INTERNAL
-}
 
 
