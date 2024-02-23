d034460493ae51ec9b07980955d3410bcfa9fe1b
23-Pravallika <learngain23@gmail.com>
2024-02-23 17:14:52 +0530

    AutoPush

--- listener.tf
+++ listener.tf
@@ -1,6 +1,6 @@
 resource "aws_lb_listener" "private" {
   count             =  var.ALB_INTERNAL ? 1 : 0
-  load_balancer_arn = data.terraform_remote_state.alb.PRIVATE_ALB_ARN
+  load_balancer_arn = aws_lb.alb.arn
   port              = "80"
   protocol          = "HTTP"
 
