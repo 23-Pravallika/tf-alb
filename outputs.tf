3634598ba476b5ee8e6f4a4952be9ec6c83d4226
23-Pravallika <learngain23@gmail.com>
2024-02-23 11:36:00 +0530

    AutoPush

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,13 @@
 output "ALB_ARN"{
     value = aws_lb.alb.arn
 }
+
+output "PUBLIC_ALB_INTERNAL" {
+    value = aws_lb.alb.ALB_INTERNAL
+}
+
+output "PRIVATE_ALB_INTERNAL" {
+    value = aws_lb.alb.ALB_INTERNAL
+}
+
+
